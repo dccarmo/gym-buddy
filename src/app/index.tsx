0414e403ac77c773554dc3c1c845cfd50afb9eba
Diogo Carmo <dccarmo@gmail.com>
2025-09-13 17:25:24 -0300

    feat: added add button

---
+++ src/app/index.tsx
@@ -0,0 +1,94 @@
+import { LiquidGlassView } from "@callstack/liquid-glass";
+import Feather from "@expo/vector-icons/Feather";
+import { Stack } from "expo-router";
+import React from "react";
+import { Platform, Pressable, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet, UnistylesRuntime } from "react-native-unistyles";
+
+export default function Screen() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Stack.Screen
+        options={{
+          header: () => null,
+        }}
+      />
+      <AddButton onPress={() => {}} />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+interface AddButtonProps {
+  onPress: () => void;
+}
+
+function AddButton({ onPress }: AddButtonProps) {
+  const theme = UnistylesRuntime.getTheme();
+
+  if (Platform.OS === "ios") {
+    return (
+      <Pressable style={addButtonStyles.container} onPress={onPress}>
+        <LiquidGlassView
+          style={addButtonStyles.content}
+          tintColor={theme.colors.primary}
+          interactive
+        >
+          <Feather name="plus" size={28} color={theme.colors.white} />
+        </LiquidGlassView>
+      </Pressable>
+    );
+  }
+
+  return (
+    <Pressable
+      style={addButtonStyles.container}
+      onPress={onPress}
+      android_ripple={{
+        color: "#FFFFFF50",
+        radius: 56,
+        foreground: true,
+        borderless: false,
+      }}
+    >
+      <View style={addButtonStyles.content}>
+        <Feather name="plus" size={28} color={theme.colors.white} />
+      </View>
+    </Pressable>
+  );
+}
+
+const addButtonStyles = StyleSheet.create((theme) => ({
+  container: {
+    position: "absolute",
+    bottom: 40,
+    right: 40,
+    ...(Platform.OS === "android" && {
+      overflow: "hidden",
+      borderRadius: 14,
+      elevation: 4,
+    }),
+  },
+  content: {
+    alignItems: "center",
+    justifyContent: "center",
+    ...(Platform.OS === "ios"
+      ? {
+          width: 50,
+          height: 50,
+          borderRadius: 25,
+        }
+      : {
+          width: 56,
+          height: 56,
+          backgroundColor: theme.colors.primary,
+          borderRadius: 14,
+        }),
+  },
+}));
---
+++ src/unistyles.ts
@@ -0,0 +1,38 @@
+import { StyleSheet } from "react-native-unistyles";
+
+export const lightTheme = {
+  colors: {
+    white: "white",
+    primary: "#00a6f4",
+  },
+};
+
+export const themes = {
+  light: lightTheme,
+};
+
+export const breakpoints = {
+  xs: 0,
+  sm: 300,
+  md: 500,
+  lg: 800,
+  xl: 1200,
+};
+
+export type Breakpoints = typeof breakpoints;
+export type Themes = typeof themes;
+
+declare module "react-native-unistyles" {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  export interface UnistylesThemes extends Themes {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  export interface UnistylesBreakpoints extends Breakpoints {}
+}
+
+StyleSheet.configure({
+  settings: {
+    initialTheme: "light",
+  },
+  breakpoints,
+  themes,
+});
